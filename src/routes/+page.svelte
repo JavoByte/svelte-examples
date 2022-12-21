1966c9a49c123723ee16ad5855950dbc2ec23669
Javier Lávida <jlavida@netflix.com>
2022-12-21 16:03:36 -0600

    feat: add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto mt-8">
+  <h1 class="text-4xl mb-4">Welcome to SvelteKit</h1>
+  <p>Visit <a class="text-blue-600 hover:text-blue-700 hover:underline" rel="noopener noreferrer" target="_blank" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
